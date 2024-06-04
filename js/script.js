73be64390487764dc7c7cdcb507b6b91a3071f1a
JesseAndersenGithub <eaa23jsoa@students.eaaa.dk>
2024-06-04 02:44:17 +0200

    pre review

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 document.getElementById('burger-menu').addEventListener('click', function() {
     document.getElementById('nav-links').classList.toggle('show');
 });
+
+document.querySelectorAll('.dropdown > a').forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+        const dropdownContent = this.nextElementSibling;
+        dropdownContent.classList.toggle('show');
+    });
+});
